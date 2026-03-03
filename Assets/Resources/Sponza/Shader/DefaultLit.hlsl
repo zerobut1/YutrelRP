6e6df9b71fd7a72d739de6b77e232a178df2bc07
zerobut1 <1181948754@qq.com>
2026-03-03 10:53:13 +0800

    Sponza 导入

---
+++ Assets/Resources/Sponza/Shader/DefaultLit.hlsl
@@ -0,0 +1,119 @@
+#ifndef YUTREL_DEFAULTLIT_INCLUDED
+#define YUTREL_DEFAULTLIT_INCLUDED
+
+#include "Assets/YutrelRP/Shader/Utils/GBuffer.hlsl"
+
+
+struct Attributes
+{
+    float3 position_OS : POSITION;
+    float3 normal_OS : NORMAL;
+    float4 tangent_OS : TANGENT;
+    float2 uv : TEXCOORD0;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float4 position_CS : SV_POSITION;
+    float3 normal_WS : VAR_NORMAL;
+    float3 tangent_WS : VAR_TANGENT;
+    float3 bitangent_WS : VAR_BITANGENT;
+    float2 uv : VAR_BASE_UV;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct RTStruct
+{
+    float4 scene_color : SV_Target0;
+    float4 GBuffer_A : SV_Target1;
+    float4 GBuffer_B : SV_Target2;
+    float4 GBuffer_C : SV_Target3;
+};
+
+Varyings DefaultLitVertex(Attributes input)
+{
+    Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_TRANSFER_INSTANCE_ID(input, output);
+    float3 position_WS = TransformObjectToWorld(input.position_OS.xyz);
+    float4 position_CS = TransformWorldToHClip(position_WS);
+    float3 normal_WS = TransformObjectToWorldNormal(input.normal_OS);
+    float3 tangent_WS = normalize(TransformObjectToWorldDir(input.tangent_OS.xyz));
+    float tangent_sign = input.tangent_OS.w * GetOddNegativeScale();
+    float3 bitangent_WS = normalize(cross(normal_WS, tangent_WS) * tangent_sign);
+
+    output.position_CS = position_CS;
+    output.normal_WS = normal_WS;
+    output.tangent_WS = tangent_WS;
+    output.bitangent_WS = bitangent_WS;
+    output.uv = input.uv;
+    return output;
+}
+
+RTStruct DefaultLitFragment(Varyings input)
+{
+    RTStruct output;
+    UNITY_SETUP_INSTANCE_ID(input);
+
+    GBufferData gbuffer;
+#if defined(_USE_BASECOLOR_TEX)
+    float4 base_color_ST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColorTex_ST);
+    float2 base_color_uv = input.uv * base_color_ST.xy + base_color_ST.zw;
+    gbuffer.base_color = SAMPLE_TEXTURE2D(_BaseColorTex, sampler_BaseColorTex, base_color_uv).rgb;
+#else
+    gbuffer.base_color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor).rgb;
+#endif
+
+#if defined(_USE_EMISSIVE_TEX)
+    float4 emissive_ST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _EmissiveTex_ST);
+    float2 emissive_uv = input.uv * emissive_ST.xy + emissive_ST.zw;
+    gbuffer.emissive = SAMPLE_TEXTURE2D(_EmissiveTex, sampler_EmissiveTex, emissive_uv).rgb;
+#else
+    gbuffer.emissive = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Emissive).rgb;
+#endif
+
+#if defined(_USE_NORMAL_TEX)
+    float4 normal_ST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _NormalTex_ST);
+    float2 normal_uv = input.uv * normal_ST.xy + normal_ST.zw;
+    float4 packed_normal = SAMPLE_TEXTURE2D(_NormalTex, sampler_NormalTex, normal_uv);
+    float3 normal_TS = UnpackNormal(packed_normal);
+    gbuffer.normal_WS = normalize(
+        normal_TS.x * input.tangent_WS +
+        normal_TS.y * input.bitangent_WS +
+        normal_TS.z * input.normal_WS
+    );
+#else
+    gbuffer.normal_WS = input.normal_WS;
+#endif
+
+#if defined(_USE_ROUGHNESS_TEX)
+    float4 roughness_ST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _RoughnessTex_ST);
+    float2 roughness_uv = input.uv * roughness_ST.xy + roughness_ST.zw;
+    gbuffer.roughness = SAMPLE_TEXTURE2D(_RoughnessTex, sampler_RoughnessTex, roughness_uv).r;
+    gbuffer.roughness = saturate(1.0f - gbuffer.roughness); // smoothness to roughness
+#else
+    gbuffer.roughness = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Roughness);
+#endif
+
+#if defined(_USE_METALLIC_TEX)
+    float4 metallic_ST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _MetallicTex_ST);
+    float2 metallic_uv = input.uv * metallic_ST.xy + metallic_ST.zw;
+    gbuffer.metallic = SAMPLE_TEXTURE2D(_MetallicTex, sampler_MetallicTex, metallic_uv).r;
+#else
+    gbuffer.metallic = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Metallic);
+#endif
+
+    gbuffer.specular = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Specular);
+    gbuffer.shading_model_id = 1;
+
+    EncodedGBuffer encoded_gbuffer = EncodeGBuffer(gbuffer);
+    output.scene_color = encoded_gbuffer.scene_color;
+    output.GBuffer_A = encoded_gbuffer.GBuffer_A;
+    output.GBuffer_B = encoded_gbuffer.GBuffer_B;
+    output.GBuffer_C = encoded_gbuffer.GBuffer_C;
+
+    return output;
+}
+
+#endif
---
+++ Assets/Resources/Sponza/Shader/Sponza_DefaultLit.shader
@@ -0,0 +1,62 @@
+﻿Shader "YutrelRP/Sponza/DefaultLit"
+{
+	Properties
+	{
+		_BaseColor ("Base Color", Color) = (0.4, 0.8, 1.0, 1)
+		_Emissive ("Emissive", Color) = (0, 0, 0, 1)
+		[Toggle(_USE_EMISSIVE_TEX)] _UseEmissiveTex ("Use Emissive Texture", Float) = 0
+		_EmissiveTex ("Emissive", 2D) = "black" {}
+		_Roughness ("Roughness", Range(0, 1)) = 0.5
+		_Metallic ("Metallic", Range(0, 1)) = 0.0
+		_Specular ("Specular", Range(0, 1)) = 0.5
+		[Toggle(_USE_BASECOLOR_TEX)] _UseBaseColorTex ("Use BaseColor Texture", Float) = 0
+		_BaseColorTex ("Base Color", 2D) = "white" {}
+		[Toggle(_USE_NORMAL_TEX)] _UseNormalTex ("Use Normal Texture", Float) = 0
+		_NormalTex ("Normal", 2D) = "bump" {}
+		[Toggle(_USE_ROUGHNESS_TEX)] _UseRoughnessTex ("Use Roughness Texture", Float) = 0
+		_RoughnessTex ("Roughness", 2D) = "white" {}
+		[Toggle(_USE_METALLIC_TEX)] _UseMetallicTex ("Use Metallic Texture", Float) = 0
+		_MetallicTex ("Metallic", 2D) = "black" {}
+	}
+	SubShader
+	{
+
+		HLSLINCLUDE
+		#include "Assets/YutrelRP/Shader/Utils/Common.hlsl"
+		#include "DefaultLitInput.hlsl"
+		ENDHLSL
+
+		Pass
+		{
+			Tags { "LightMode" = "GBuffer" }
+
+			HLSLPROGRAM
+			#pragma target 5.0
+			#pragma multi_compile_instancing
+			#pragma shader_feature_local _USE_BASECOLOR_TEX
+			#pragma shader_feature_local _USE_EMISSIVE_TEX
+			#pragma shader_feature_local _USE_NORMAL_TEX
+			#pragma shader_feature_local _USE_ROUGHNESS_TEX
+			#pragma shader_feature_local _USE_METALLIC_TEX
+			#pragma vertex DefaultLitVertex
+			#pragma fragment DefaultLitFragment
+			#include "DefaultLit.hlsl"
+			ENDHLSL
+		}
+
+		Pass
+		{
+			Tags { "LightMode" = "ShadowCaster" }
+
+			ColorMask 0
+
+			HLSLPROGRAM
+			#pragma target 5.0
+			#pragma multi_compile_instancing
+			#pragma vertex ShadowCasterPassVertex
+			#pragma fragment ShadowCasterPassFragment
+			#include "Assets/YutrelRP/Shader/ShadowCaster.hlsl"
+			ENDHLSL
+		}
+	}
+}
